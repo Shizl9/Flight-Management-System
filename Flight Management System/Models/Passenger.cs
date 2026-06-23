defe35b1de9548e38f4c353e7ebaeab0fc68b97e
Sheika <sheika02x2@gmail.com>
2026-06-23 14:53:25 +0400

    adding class of Passenger

---
+++ Flight Management System/Models/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Management_System.Models
+{
+    public class Passenger
+    {
+        public int passengerId { get; set; } //syste generated 
+        public string passengerName { get; set;  } // input by passenger
+        public string passengerEmail { get; set; }//input by passenger
+        public string passengerPhone { get; set; }//input by passenger
+        public string passportNumber { get; set; }//input by passenger
+        public string nationality { get; set; }//input by passenger
+    }
+}
