047987f0cb7aa8c035503fd1610c31ecf2f6786c
Sheika <sheika02x2@gmail.com>
2026-06-23 15:33:20 +0400

    adding classes of pailot , aircraft

---
+++ Flight Management System/Models/Aircraft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Management_System.Models
+{
+    public class Aircraft
+    {
+        public int aircraftId { get; set; } //syste generated 
+        public string model { get; set; } // input by Pilot
+        public string pilotPhone { get; set; }//input by Pilot
+        public string licenseNumber { get; set; }//input by Pilot
+    }
+}
---
+++ Flight Management System/Models/Pilot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Management_System.Models
+{
+    public class Pilot
+    {
+        public int pilotId { get; set; } //syste generated 
+        public string pilotName { get; set; } // input by Pilot
+        public string pilotPhone { get; set; }//input by Pilot
+        public string licenseNumber { get; set; }//input by Pilot
+        public int flightHours { get; set; }//calculated from every flights
+        public bool isAvailable { get; set; }// default value set as true when pilot adedd.
+    }
+}
